01e57b1039ebf6a65c7a955c4a73e572c0de83a8
Praveen Baithi <pbaithi@osius.com>
2025-01-31 10:21:19 +0530

    Your commit message

---
+++ src/test/java/pbaithi/aws/docker/AwsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package pbaithi.aws.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
